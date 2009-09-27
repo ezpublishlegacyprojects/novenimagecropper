013428eddeae228efba6cc4812c1d6021610b5b5
lolautruche <info@example.com>
2009-09-27 09:07:50 +0000

    Tag version 1.0 final
    git-svn-id: http://svn.projects.ez.no/novenimagecropper@12 3b5cbe22-813a-4614-bd81-d6c648d41210

---
+++ tags/1.0/settings/ezjscore.ini.append.php
@@ -0,0 +1,11 @@
+<?php /* #?ini charset="utf-8"?
+
+[eZJSCore]
+PreferredLibrary=jquery
+
+[ezjscServer_novimgcrop]
+# Url to test this server function(return current time):
+# <root>/ezjscore/call/novimgcrop::imageReference::<AttributeID>::<ContentObjectVersion>::<ContentObjectID>
+Class=NovImgCropServerFunctions
+
+*/

d553f2e52a497886eddeec7ff4ebdf96cb83b8f5
lolautruche <info@example.com>
2009-10-07 15:37:07 +0000

    German translation updated (thanks Marco Zinn !)
    git-svn-id: http://svn.projects.ez.no/novenimagecropper@37 3b5cbe22-813a-4614-bd81-d6c648d41210

--- trunk/translations/ger-DE/translation.ts
+++ trunk/translations/ger-DE/translation.ts
@@ -2,53 +2,57 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="de_DE" sourcelanguage="en">
 <context>
-   <name>extension/novenimagecropper</name>
-   <message>
-       <source>Crop image</source>
-       <translation>Bild zuschneiden</translation>
-   </message>
-   <message>
-       <source>Image cropping interface</source>
-       <translation>Oberfläche &quot;Bild zuschneiden&quot;</translation>
-   </message>
-   <message>
-       <source>Aspect Ratio</source>
-       <translation>Seitenverhältnis</translation>
-   </message>
-   <message>
-       <source>No aspect ratio</source>
-       <translation>Kein Seitenverhältnis</translation>
-   </message>
-   <message>
-       <source>Preview</source>
-       <translation>Vorschau</translation>
-   </message>
-   <message>
-       <source>Cancel</source>
-       <translation>Abbruch</translation>
-   </message>
-   <message>
-       <source>Back to selection</source>
-       <translation>Zurück zur Auswahl</translation>
-   </message>
-   <message>
-       <source>Save</source>
-       <translation>Speichern</translation>
-   </message>
+    <name>extension/novenimagecropper</name>
+    <message>
+        <source>Crop image</source>
+        <translation>Bild zuschneiden</translation>
+    </message>
+    <message>
+        <source>Image cropping interface</source>
+        <translation>Bildausschnitt festlegen</translation>
+    </message>
+    <message>
+        <source>Aspect Ratio</source>
+        <translation>Seitenverhältnis</translation>
+    </message>
+    <message>
+        <source>No aspect ratio</source>
+        <translation>beliebig</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Vorschau</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Abbruch</translation>
+    </message>
+    <message>
+        <source>Back to selection</source>
+        <translation>Ausschnitt ändern</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
 </context>
 <context>
-   <name>extension/novenimagecropper/error</name>
-   <message>
-       <source>Invalid crop mode</source>
-       <translation>Ungülter Zuschnitt-Modus</translation>
-   </message>
-   <message>
-       <source>Please make a selection to crop your image</source>
-       <translation>Bitte wählen Sie etwas aus, um das Bild zuzuschneiden</translation>
-   </message>
-   <message>
-       <source>Uploaded file is not a supported image file (PNG/JPG/GIF)</source>
-       <translation>Dateityp der hochgeladener Datei ist kein unterstützes Bildformat (PNG/JPG/GIF)</translation>
-   </message>
+    <name>extension/novenimagecropper/error</name>
+    <message>
+        <source>Invalid crop mode</source>
+        <translation>Ungülter Zuschnitt-Modus</translation>
+    </message>
+    <message>
+        <source>Please make a selection to crop your image</source>
+        <translation>Bitte wählen Sie einen Bereich aus, um das Bild zuzuschneiden</translation>
+    </message>
+    <message>
+        <source>Uploaded file is not a supported image file (PNG/JPG/GIF)</source>
+        <translation>Dateityp der hochgeladener Datei ist kein unterstützes Bildformat (PNG/JPG/GIF)</translation>
+    </message>
+    <message>
+        <source>Neither ImageMagick nor GD image handler is enabled ! Please check your image.ini configuration</source>
+        <translation>Weder für ImageMagick noch für GD ist der ImageHandler aktiv! Bitte prüfen Sie die Konfiguration in der image.ini</translation>
+    </message>
 </context>
 </TS>

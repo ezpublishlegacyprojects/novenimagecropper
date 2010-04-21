a2ec7aa5d2953f4f381ace3a26b0e9b7f1223cb2
lolautruche <info@example.com>
2010-04-21 20:50:17 +0000

    Added Norwegian translation (thanks Håvard Bergersen for your contribution !)
    git-svn-id: http://svn.projects.ez.no/novenimagecropper@51 3b5cbe22-813a-4614-bd81-d6c648d41210

---
+++ trunk/translations/nor-NO/translation.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="no_NB" sourcelanguage="en">
+<context>
+    <name>extension/novenimagecropper</name>
+    <message>
+        <source>Crop image</source>
+        <translation>Beskjær bildet</translation>
+    </message>
+    <message>
+        <source>Image cropping interface</source>
+        <translation>Bildebeskjærings grensesnitt</translation>
+    </message>
+    <message>
+        <source>Aspect Ratio</source>
+        <translation>Størrelsesforhold</translation>
+    </message>
+    <message>
+        <source>No aspect ratio</source>
+        <translation>Ikke noe størrelsesforhold</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Forhåndsvis</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>Back to selection</source>
+        <translation>Tilbake til seleksjon</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Lagre</translation>
+    </message>
+</context>
+<context>
+    <name>extension/novenimagecropper/error</name>
+    <message>
+        <source>Invalid crop mode</source>
+        <translation>Ugyldig beskjæringsmetode</translation>
+    </message>
+    <message>
+        <source>Please make a selection to crop your image</source>
+        <translation>Vennligst velg et område for å beskjære bildet</translation>
+    </message>
+    <message>
+        <source>Uploaded file is not a supported image file (PNG/JPG/GIF)</source>
+        <translation>Bildet du har lastet opp er i et ugyldig filformat (PNG/JPG/GIF)</translation>
+    </message>
+    <message>
+        <source>Neither ImageMagick nor GD image handler is enabled ! Please check your image.ini configuration</source>
+        <translation>Verken ImageMagick eller GD image handler er aktivert. Vennligst sjekk innstillingene i image.ini.</translation>
+    </message>
+</context>
+</TS>

9c5480a3dcdab26821f14aa10abb5d3bd7a5e6e2
lolautruche <info@example.com>
2010-04-25 08:58:56 +0000

    Added spanish translation (thanks Carlos Revillo)
    git-svn-id: http://svn.projects.ez.no/novenimagecropper@52 3b5cbe22-813a-4614-bd81-d6c648d41210

---
+++ trunk/translations/esl-ES/translation.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES" sourcelanguage="en">
+<context>
+    <name>extension/novenimagecropper</name>
+    <message>
+        <source>Crop image</source>
+        <translation>Recortar imagen</translation>
+    </message>
+    <message>
+        <source>Image cropping interface</source>
+        <translation>Interfaz para recortar imágenes</translation>
+    </message>
+    <message>
+        <source>Aspect Ratio</source>
+        <translation>Proporción</translation>
+    </message>
+    <message>
+        <source>No aspect ratio</source>
+        <translation>Sin proporción</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Previsualizar</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Back to selection</source>
+        <translation>Vovler a la selección</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+</context>
+<context>
+    <name>extension/novenimagecropper/error</name>
+    <message>
+        <source>Invalid crop mode</source>
+        <translation>Modo de recorte inválido</translation>
+    </message>
+    <message>
+        <source>Please make a selection to crop your image</source>
+        <translation>Por favor, selecciona para recortar tu imagen</translation>
+    </message>
+    <message>
+        <source>Uploaded file is not a supported image file (PNG/JPG/GIF)</source>
+        <translation>El fichero subido no es una fichero de imagen soportado (PNG/JPG/GIF)</translation>
+    </message>
+    <message>
+        <source>Neither ImageMagick nor GD image handler is enabled ! Please check your image.ini configuration</source>
+        <translation>Ni el controloador de imagen ImageMagick ni el de GD parecen estar activados. Por favor revisa la configuración de tu image.ini</translation>
+    </message>
+</context>
+</TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES" sourcelanguage="en">
+<context>
+    <name>extension/novenimagecropper</name>
+    <message>
+        <source>Crop image</source>
+        <translation>Recortar imagen</translation>
+    </message>
+    <message>
+        <source>Image cropping interface</source>
+        <translation>Interfaz para recortar imágenes</translation>
+    </message>
+    <message>
+        <source>Aspect Ratio</source>
+        <translation>Proporción</translation>
+    </message>
+    <message>
+        <source>No aspect ratio</source>
+        <translation>Sin proporción</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Previsualizar</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Back to selection</source>
+        <translation>Vovler a la selección</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+</context>
+<context>
+    <name>extension/novenimagecropper/error</name>
+    <message>
+        <source>Invalid crop mode</source>
+        <translation>Modo de recorte inválido</translation>
+    </message>
+    <message>
+        <source>Please make a selection to crop your image</source>
+        <translation>Por favor, selecciona para recortar tu imagen</translation>
+    </message>
+    <message>
+        <source>Uploaded file is not a supported image file (PNG/JPG/GIF)</source>
+        <translation>El fichero subido no es una fichero de imagen soportado (PNG/JPG/GIF)</translation>
+    </message>
+    <message>
+        <source>Neither ImageMagick nor GD image handler is enabled ! Please check your image.ini configuration</source>
+        <translation>Ni el controloador de imagen ImageMagick ni el de GD parecen estar activados. Por favor revisa la configuración de tu image.ini</translation>
+    </message>
+</context>
+</TS>

a618f051e76abe10e11f41250238457443687f88
Gaetano Giunta <gg@ez.no>
2010-04-27 20:29:23 +0000

    - added italian translation
    git-svn-id: http://svn.projects.ez.no/novenimagecropper@53 3b5cbe22-813a-4614-bd81-d6c648d41210

---
+++ trunk/translations/ita-IT/translation.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ita_IT" sourcelanguage="en">
+<context>
+    <name>extension/novenimagecropper</name>
+    <message>
+        <source>Crop image</source>
+        <translation>Ritaglia immagine</translation>
+    </message>
+    <message>
+        <source>Image cropping interface</source>
+        <translation>Interfaccia di ritaglio immagini</translation>
+    </message>
+    <message>
+        <source>Aspect Ratio</source>
+        <translation>Rapporto A:L</translation>
+    </message>
+    <message>
+        <source>No aspect ratio</source>
+        <translation>Nessun rapporto A:L</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Previsualizzazione</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>Back to selection</source>
+        <translation>Torna alla selezione</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Salva</translation>
+    </message>
+</context>
+<context>
+    <name>extension/novenimagecropper/error</name>
+    <message>
+        <source>Invalid crop mode</source>
+        <translation>Modo di ritaglio non valido</translation>
+    </message>
+    <message>
+        <source>Please make a selection to crop your image</source>
+        <translation>Selezionare un'area per ritagliare l'immagine</translation>
+    </message>
+    <message>
+        <source>Uploaded file is not a supported image file (PNG/JPG/GIF)</source>
+        <translation>Il file caricato non é un tipo di immagine supportato (PNG/JPG/GIF)</translation>
+    </message>
+    <message>
+        <source>Neither ImageMagick nor GD image handler is enabled ! Please check your image.ini configuration</source>
+        <translation>Ne il gestore di immagini ImageMagick ne quello GD sono attivati ! Verificare la configurazione nel file image.ini</translation>
+    </message>
+</context>
+</TS>
